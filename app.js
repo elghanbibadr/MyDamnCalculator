237b9216fa05baba58872a6150a0c65e85c74e00
badr <bghanbi50@gmail.com>
2022-03-24 15:34:05 +0100

    finish

---
+++ app.js
@@ -0,0 +1,30 @@
+const inputBill=document.querySelector('#bill-input');
+const tipBtns=document.querySelectorAll('.btn--primary');
+const numberOfPeople=document.querySelector('#numbersOfPeople-input');
+const tipAmount=document.querySelector('.tipPerPerson');
+const total=document.querySelector('.totalPerPerson');
+const reset=document.querySelector('.reset-btn');
+// specifie the clicked button
+tipBtns.forEach(btn=>{
+   btn.addEventListener('click',(e)=>{
+    tipBtns.forEach(btn=>btn.style.backgroundColor='hsl(183, 100%, 15%)')
+      e.target.style.backgroundColor='hsl(172, 67%, 45%)';
+     tipAmount.textContent=((parseInt(e.target.textContent)/100)*inputBill.value);
+     updateAmountValue();
+   });
+
+});
+
+     inputBill.addEventListener('input',updateAmountValue);
+     numberOfPeople.addEventListener('input',updateAmountValue)
+ // update amount value
+  function updateAmountValue(){
+     if (numberOfPeople.value==0) return;
+   
+         total.textContent=(inputBill.value/numberOfPeople.value)+ parseInt(tipAmount.textContent); 
+   }
+   // reset
+
+  reset.addEventListener('click',()=>{
+   window.location.reload();
+  })
